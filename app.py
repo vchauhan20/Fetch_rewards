e348a48a7ffeb4f9e48744ce1b8a622b5de7b442
vchauhan20 <47049327+vchauhan20@users.noreply.github.com>
2020-12-15 21:47:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     return render_template("home.html")
 
 @app.route('/predict',methods=['POST'])
-def predict(stop_usage =1):
+def calculate():
     texts = [x for x in request.form.values()]
 
     first = TextSimilarity(texts[0].lower().strip())
@@ -27,10 +27,11 @@ def predict(stop_usage =1):
     first_second_similarity = first.cosine_similar_score(second)
     second_third_similarity = second.cosine_similar_score(third)
     first_third_similarity = first.cosine_similar_score(third)
-    result = f'first --> Second {round(first_second_similarity,3)   }   '+ \
-            f'   Second --> Third {round(second_third_similarity,3) }   ' + \
-            f'   First --> Third  {round(first_third_similarity,3)  }'
-    return render_template('home.html',result=result)
+
+    result_12 = f'first --> Second Similarity :{round(first_second_similarity,3)   }'
+    result_23 = f' Second --> Third Similarity :{round(second_third_similarity,3) } '
+    result_13 = f' First --> Third  Similarity :{round(first_third_similarity,3)  }'
+    return render_template('home.html',result_12=result_12 , result_23 = result_23 , result_13 = result_13)
 
 if __name__ == '__main__':
     app.run(debug=True)
