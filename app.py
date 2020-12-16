82c4cc01cf3bcea2c5ba48750519c552e39ed596
vchauhan20 <47049327+vchauhan20@users.noreply.github.com>
2020-12-15 20:50:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,6 @@ def predict(stop_usage =1):
     second.remove_stop_words(stop_usage)
     third.remove_stop_words(stop_usage)
 
-    print(first.cleaned_tokens)
-    print(second.cleaned_tokens)
-    print(third.cleaned_tokens)
 
     first_second_similarity = first.cosine_similar_score(second)
     second_third_similarity = second.cosine_similar_score(third)
